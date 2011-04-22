6e1889421aa379579c5d885a61ebf0555ecb17c9
Tom Hornos <tom.hornos@gmail.com>
2011-04-22 18:36:43 +0200

    dm

---
+++ apple/depesmód.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#/// \file liszt.sh
+#/// \brief az OS X alaphangjainak beállítása
+#
+
+for d in "Dropbox" "Dropbox.client" "Private" ; do
+  _p="${HOME}/${d}"
+  if test -d "${_p}" ; then
+    chmod -R g-wrx "${_p}"
+    chmod -R o-wrx "${_p}"
+  fi
+done
